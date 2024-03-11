73d708a9778a10191bf5da20679c41279b16aebe
cccaternberg <acaternberg@cloudbees.com>
2024-03-11 17:42:00 +0100

    add

--- vars/init.groovy
+++ vars/init.groovy
@@ -8,6 +8,6 @@ def call(String configFileName) {
         //see https://docs.cloudbees.com/docs/cloudbees-ci/latest/automating-with-jenkinsfile/customizing-parameters
         evaluate(valuesYaml.properties)
     } catch (Exception e) {
-        println "FileNotFound './ci.yaml'"
+        println "FileNotFound ${configFileName}"
     }
 }
