2fdb342dcd8d60de4bba3d6fd9be418286bc3657
cccaternberg <acaternberg@cloudbees.com>
2024-03-12 09:35:10 +0100

    add

--- vars/defineProps.groovy
+++ vars/defineProps.groovy
@@ -1,9 +1,6 @@
 // vars/defineProps.groovy
 def call(String file, Map defaults) {
-  //defaults from job name for Docker image
-  //imageNameTag()
-  //set SHORT_COMMIT for all defineProps calls
-  gitShortCommit()
+
   //use the Pipeline Utility Steps plugin readProperties step to read the .<app>.properties custom marker file
   def props = readProperties defaults: defaults, file: file
   for ( e in props ) {
