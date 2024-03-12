49dc669a8fd249390256977342f7e8624f9ce23f
cccaternberg <acaternberg@cloudbees.com>
2024-03-12 09:50:58 +0100

    add

--- vars/pipelineHelloWorld.groovy
+++ vars/pipelineHelloWorld.groovy
@@ -12,13 +12,19 @@ def call(Map pipelineParams) {
         stages {
             stage("Init") {
                 steps {
-                    init "./ci-config.yaml"
+                    //Init from yamnl
+                    // init "./ci-config.yaml"
+                    //Init from marker properties with defaults   (here ansible tower f.e)
+                    defineProps('ci-config.properties', [ansibletower_async: 'true'])
                 }
             }
             stage('Say Hello') {
+                when ()
                 steps {
                     echo "Greetings: ${params.greeting}"
                     echo "${pipelineParams.app}"
+                    sh "echo ansibletower_async ${env.ansibletower_async}"
+                    sh "echo key1 ${env.key1}"
                 }
             }
         }
