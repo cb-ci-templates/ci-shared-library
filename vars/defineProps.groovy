2c994023db16cfc44d1aa9689e0bebdf19961caf
cccaternberg <acaternberg@cloudbees.com>
2024-03-12 09:46:32 +0100

    add

--- vars/defineProps.groovy
+++ vars/defineProps.groovy
@@ -1,6 +1,5 @@
 // vars/defineProps.groovy
 def call(String file, Map defaults) {
-
   //use the Pipeline Utility Steps plugin readProperties step to read the .<app>.properties custom marker file
   def props = readProperties defaults: defaults, file: file
   for ( e in props ) {
