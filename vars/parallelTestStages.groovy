a4edfe9877c65bac3431d39bb974a587fe42c969
cccaternberg <acaternberg@cloudbees.com>
2024-03-13 19:43:31 +0100

    add

--- vars/parallelTestStages.groovy
+++ vars/parallelTestStages.groovy
@@ -1,9 +1,13 @@
 // Convert array of regions to map of stages
 def call(stages) {
+        //TODO: we need a 2 dimensional array
+        //[stage1][testparams1]
+        //[stageX][testparamsX]
         stageList=stages.collectEntries { mystage ->
             [
                     (mystage): {
-                        stage("Deploy region ${mystage}") {
+                        stage("${mystage}") {
+                            //TODO we need the test params here
                             echo  "sample test command ${mystage}"
                         }
                     }
