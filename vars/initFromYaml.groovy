2527bdc30ce92cad54fe568a396aee8ca40254d7
cccaternberg <acaternberg@cloudbees.com>
2024-03-13 14:49:43 +0100

    update

--- vars/initFromYaml.groovy
+++ vars/initFromYaml.groovy
@@ -7,6 +7,12 @@ def call(String configFileName) {
         //generate properties, global options and parameters
         //see https://docs.cloudbees.com/docs/cloudbees-ci/latest/automating-with-jenkinsfile/customizing-parameters
         evaluate(valuesYaml.properties)
+
+        //generate global environment vars
+        valuesYaml.environment.each { environmentVar ->
+            evaluate("env."+environmentVar)
+        }
+
     } catch (Exception e) {
         println "FileNotFound ${configFileName}"
     }
