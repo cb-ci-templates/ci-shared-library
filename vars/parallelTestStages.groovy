c19ac2d36b698d1cc066df46369619bb8a596e1f
cccaternberg <acaternberg@cloudbees.com>
2024-03-15 09:49:12 +0100

    add

--- vars/parallelTestStages.groovy
+++ vars/parallelTestStages.groovy
@@ -1,7 +1,6 @@
 // Convert array of regions to map of stages
 def call(stages) {
-        def myStages=stages
-        stageList=myStages.collectEntries { mystage ->
+        stageList=stages.collectEntries { mystage ->
             [
                     (mystage): {
                         stage("${mystage}") {
