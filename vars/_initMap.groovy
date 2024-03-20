0042b91bffdf935e1fc1d81b420a4a0ae0b80d1c
cccaternberg <acaternberg@cloudbees.com>
2024-03-20 13:23:31 +0100

    update

--- vars/_initMap.groovy
+++ vars/_initMap.groovy
@@ -3,13 +3,15 @@
 def call(String fileName){
      // Read YAML file
     //def yamlSlurper = new YamlSlurper(new String(Files.readAllBytes(Paths.get(fileName))))
-
+    sh "ls -lR"
     // Parse YAML content
     //def yamlMap = yamlSlurper.parseText(yamlText)
-    node(){
+    /*node(){
         Map yamlMap = readYaml(file: fileName)
         return yamlMap
     }
+    */
+
 
     // Print the Map
     println(yamlMap)
