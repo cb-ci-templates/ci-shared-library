f7e15423763d56e691751743a8197d79d44253de
cccaternberg <acaternberg@cloudbees.com>
2024-03-20 13:19:19 +0100

    update

--- vars/_initMap.groovy
+++ vars/_initMap.groovy
@@ -1,12 +1,12 @@
-import groovy.yaml.YamlSlurper
+//import groovy.yaml.YamlSlurper
 
 def call(String fileName){
      // Read YAML file
-    def yamlSlurper = new YamlSlurper(new String(Files.readAllBytes(Paths.get(fileName))))
+    //def yamlSlurper = new YamlSlurper(new String(Files.readAllBytes(Paths.get(fileName))))
 
     // Parse YAML content
-    def yamlMap = yamlSlurper.parseText(yamlText)
-
+    //def yamlMap = yamlSlurper.parseText(yamlText)
+    Map yamlMap = readYaml(file: fileName)
     // Print the Map
     println(yamlMap)
 
