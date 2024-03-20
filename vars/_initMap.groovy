cef8d4b180e1567e24388d97b8d7faad58340473
cccaternberg <acaternberg@cloudbees.com>
2024-03-20 12:53:52 +0100

    update

--- vars/_initMap.groovy
+++ vars/_initMap.groovy
@@ -1,20 +1,15 @@
-@Grab(group='org.yaml', module='snakeyaml', version='1.29')
-import org.yaml.snakeyaml.Yaml
-import java.nio.file.Files
-import java.nio.file.Paths
+import groovy.yaml.YamlSlurper
 
 def call(String fileName){
-    def myMap = [:]
+     // Read YAML file
+    def yamlSlurper = new YamlSlurper(new String(Files.readAllBytes(Paths.get(fileName))))
 
-    // Read YAML file
-    def yamlText = new String(Files.readAllBytes(Paths.get(fileName)))
+    // Parse YAML content
+    def yamlMap = yamlSlurper.parseText(yamlText)
 
-    // Parse YAML
-    def templateYaml = new Yaml().load(yamlText)
-    templateYaml.parameters.each { property ->
-        //println("Name: ${property.name},DefaultValue: ${property.defaultValue}")
-        myMap["$property.name"] = "${property.defaultValue}"
-    }
-    println(myMap)
-    return myMap
+    // Print the Map
+    println(yamlMap)
+
+
+    return yamlMap
 }
