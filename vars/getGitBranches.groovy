f3a24ecc598c42e0a7390c71f9a340c8f22857d0
cccaternberg <acaternberg@cloudbees.com>
2024-03-22 22:05:43 +0100

    add

---
+++ vars/getGitBranches.groovy
@@ -0,0 +1,11 @@
+def call(String ghtoken,String repo){t
+    branches=sh(script: """
+            curl -s -L \
+             -H "Accept: application/vnd.github+json" \
+             -H "Authorization: Bearer ${ghtoken}" \
+             -H "X-GitHub-Api-Version: 2022-11-28" \
+             $repo  |jq -r '.[] | .name' | tr '\\n' ', ' | sed 's/,\$//'"""
+            ,returnStdout: true)
+    echo "BRANCHES: ${branches}"
+    return branches
+}
