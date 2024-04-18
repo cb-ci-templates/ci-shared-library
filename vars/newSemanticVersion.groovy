52463b55bc3074044a1dd64775778c7192ae1f56
cccaternberg <acaternberg@cloudbees.com>
2024-04-18 08:51:07 +0200

    fix

--- vars/newSemanticVersion.groovy
+++ vars/newSemanticVersion.groovy
@@ -1,9 +1,6 @@
 def call(Map params){
     writeFile encoding: 'utf-8', file: "${WORKSPACE}/nsv.sh", text: libraryResource ('scripts/newSemanticVersion.sh')
-    def newSemanticVersion=sh(script: """
-                                chmod a+x ${WORKSPACE}/nsv.sh && ${WORKSPACE}/nsv.sh ${params.arg} ${params.version}
-                            """
-                            ,returnStdout: true)
+    def newSemanticVersion=sh(script: "chmod a+x ${WORKSPACE}/nsv.sh && ${WORKSPACE}/nsv.sh ${params.arg} ${params.version}",returnStdout: true)
     echo "New Version: ${newSemanticVersion}"
     env.NEW_SEMANTIC_VERSION="${newSemanticVersion}"
     return newSemanticVersion
