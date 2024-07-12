af6c1061f9bd0cf6652905d904acc881941256ac
cccaternberg <acaternberg@cloudbees.com>
2024-07-12 15:36:58 +0200

    add

--- vars/pipeline_simple.groovy
+++ vars/pipeline_simple.groovy
@@ -20,7 +20,7 @@ def call(Map configDefaults) {
                         writeYaml file: 'ci-config-defaults.yaml', data: libraryResource("json/ci-config-defaults.yaml")
                         sh """
                             cat ci-config-defaults.yaml
-                            yq m -x ${configDefaults.branchPropertiesFile} ci-config-defaults.yaml > config-merged.yaml
+                            yq eval m  ${configDefaults.branchPropertiesFile} ci-config-defaults.yaml > config-merged.yaml
                             cat config-merged.yaml
                         """
                         script {
