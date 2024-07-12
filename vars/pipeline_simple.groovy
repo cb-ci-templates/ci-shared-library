b894c0769b49adbbd70d6a357ddf69a40dec4990
cccaternberg <acaternberg@cloudbees.com>
2024-07-12 16:08:57 +0200

    add

--- vars/pipeline_simple.groovy
+++ vars/pipeline_simple.groovy
@@ -34,11 +34,11 @@ def call(Map configDefaults) {
                     }
                    */
                     container("envsubst") {
-                        env.MAVEN_IMAGE = config.build.maven.image
-                        //env.MAVEN_IMAGE="maven:3-amazoncorretto-17"
                         writeYaml file: 'agent.yaml', data: libraryResource("podtemplates/podTemplate-envsubst-images.yaml")
-                        sh "ls -la && envsubst < agent.yaml > tmp-podagent.yaml"
-                        script {
+                        script{
+                            env.MAVEN_IMAGE = config.build.maven.image
+                            //env.MAVEN_IMAGE="maven:3-amazoncorretto-17"
+                            sh "ls -la && envsubst < agent.yaml > tmp-podagent.yaml"
                             agentYaml = readYaml file: "tmp-podagent.yaml"
                         }
                         sh "echo $agentYaml"
