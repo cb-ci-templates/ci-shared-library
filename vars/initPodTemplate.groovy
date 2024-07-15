cb88c640c9de77ccf076fef3d3e65eee83a81d55
cccaternberg <acaternberg@cloudbees.com>
2024-07-15 09:17:13 +0200

    sed

--- vars/initPodTemplate.groovy
+++ vars/initPodTemplate.groovy
@@ -4,17 +4,17 @@ def call(Map config) {
         //TODO: Iterate over all config.build.X.images and expose them as ebv vars
         env.MAVEN_IMAGE = config.build.maven.image
         //env.MAVEN_IMAGE="maven:3-amazoncorretto-17"
-        def agentYaml=libraryResource("podtemplates/podTemplate-envsubst-images.yaml")
+        writeYaml file: "agent.yaml" , data: libraryResource("podtemplates/podTemplate-envsubst-images.yaml")
         sh '''
             ls -la 
             cat agent.yaml
-            echo ${agentYaml} |envsubst > ${config.dynamicPodTemplateFile}
+            envsubst < agent.yaml  > ${config.dynamicPodTemplateFile}
             sed -i '1d' ${config.dynamicPodTemplateFile} #workartund to remove "---"
             cat ${config.dynamicPodTemplateFile}
         '''
         //writeYaml file: config.dynamicPodTemplateFile , data: libraryResource("podtemplates/podTemplate-envsubst-images.yaml")
-        result = readYaml file: "tmp-podagent.yaml"
-        sh "echo $agentYaml"
+        result = readYaml file: config.dynamicPodTemplateFile
+        sh "echo $result"
     }
     return result
 }
