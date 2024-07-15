17abab9df92665ca179bcfe69787f217496b161b
cccaternberg <acaternberg@cloudbees.com>
2024-07-15 16:14:40 +0200

    add custom yq image

--- vars/initPodTemplate.groovy
+++ vars/initPodTemplate.groovy
@@ -1,12 +1,46 @@
 def call(Map config) {
     Map agentYaml=null
     container("yq") {
-        //TODO: Iterate over all config.build.X.images and expose them as ebv vars
-        env.MAVEN_IMAGE = config.build.maven.image
+        // Define the path to your template file
+        def podTemplateFilePath = 'agentTemplate.yaml'
         //env.MAVEN_IMAGE="maven:3-amazoncorretto-17"
         //agentRef=libraryResource("podtemplates/podTemplate-envsubst-images.yaml")
-        writeYaml file: 'agentTemplate.yaml', data: libraryResource("podtemplates/podTemplate-envsubst-images.yaml")
-        sh """
+        writeYaml file: podTemplateFilePath, data: libraryResource("podtemplates/podTemplate-envsubst-images.yaml")
+        // Define the environment variables
+        def envVars = [
+                MAVEN_IMAGE = config.build.maven.image
+        ]
+     /* TOD=: itearet over configv images and add to array
+        def envVars = [
+                MAVEN_IMAGE = config.build.maven.image,
+                GRADLE_IMAGE = config.build.maven.image
+        ]
+
+      */
+
+
+
+
+
+
+// Replace variables in the file
+         agentYaml = replaceVariablesInFile(podTemplateFilePath, envVars)
+
+// Print the result or save it back to a file
+        println(agentYaml)
+        // Optionally, save the result to a new file
+// new File('output.yaml').text = result
+        writeYaml file: 'gen-agentTemplate.yaml', data: agentYaml
+
+
+
+
+
+
+
+
+
+       /* sh """
             #cat agentTemplate.yaml |envsubst |yq > gen-agentTemplate.yaml
             ls -la
             sed -i "s/^  //g" agentTemplate.yaml 
@@ -15,9 +49,9 @@ def call(Map config) {
             envsubst < agentTemplate.yaml > gen-agentTemplate.yaml
             ls -la            
          """
+
+        */
         //#sed -i '1d' tmp-podagent.yaml #workartund
-       // agentYaml = readYaml file: 'gen-agentTemplate.yaml'
-        sh "yq gen-agentTemplate.yaml"
         agentYaml = readYaml file: 'gen-agentTemplate.yaml'
         println agentYaml
         archiveArtifacts artifacts: '*.yaml', followSymlinks: false
@@ -28,4 +62,15 @@ def call(Map config) {
          */
     }
     return agentYaml
+}
+
+
+def replaceVariablesInFile(filePath, envVars) {
+    // Read the content of the file
+    def fileContent = new File(filePath).text
+    // Replace placeholders with environment variables
+    envVars.each { key, value ->
+        fileContent = fileContent.replace("\${${key}}", value)
+    }
+    return fileContent
 }
