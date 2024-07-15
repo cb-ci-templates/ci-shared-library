622744c8d530900c8335ff31167f1de5a8e25608
cccaternberg <acaternberg@cloudbees.com>
2024-07-15 18:46:40 +0200

    add custom yq image

--- vars/initPodTemplate.groovy
+++ vars/initPodTemplate.groovy
@@ -1,51 +1,31 @@
 def call(Map config) {
     def result=null
     container("yq") {
+        def replacements = [
+                '\${MAVEN_IMAGE}': config.build.maven.image ?: 'maven:3-amazoncorretto-17',
+                '\${KANIKO_IMAGE}': config.build.kaniko.image ?: 'kaniko'
+        ]
         // Define the path to your template file
         def podTemplateFilePath = 'agentTemplate.yaml'
-        //env.MAVEN_IMAGE = config.build.maven.image
-        //env.MAVEN_IMAGE="maven:3-amazoncorretto-17"
-        //agentRef=libraryResource("podtemplates/podTemplate-envsubst-images.yaml")
-        //writeYaml file: podTemplateFilePath, data: libraryResource("podtemplates/podTemplate-envsubst-images.yaml")
         def agentPod=libraryResource("podtemplates/podTemplate-envsubst-images.yaml")
-                .replace('\${MAVEN_IMAGE}',config.build.maven.image)
+        /*   .replace('\${MAVEN_IMAGE}',config.build.maven.image)
+                .replace('\${GRADLE_IMAGE}',config.build.maven.image)
+*/
+        // Function to replace tokens using a map
+
+
+// Replace tokens
+        agentPod = replaceTokens(agentPod, replacements)
         writeYaml file: podTemplateFilePath, data: agentPod
         archiveArtifacts artifacts: '*.yaml', followSymlinks: false
         return agentPod
-        /*
-        writeYaml file: podTemplateFilePath, data: libraryResource("podtemplates/podTemplate-envsubst-images.yaml")
-
-        sh """
-            set -x
-            ls -la        
-            cat ${podTemplateFilePath} |envsubst > gen-agentTemplate.yaml
-            ls -la
-            sed -i "s/^  //g" ${podTemplateFilePath}
-            sed -i '1d' ${podTemplateFilePath}
-            cat gen-agentTemplate.yaml            
-        """
-        */
-        /*
-
-            #sed -i "s/^  //g" ${podTemplateFilePath}
-            #sed -i '1d' ${podTemplateFilePath}
-            #cat ${podTemplateFilePath}
-            echo ${agentPod}
-         */
-        //archiveArtifacts artifacts: '*.yaml', followSymlinks: false
-        //return readYaml(file: "gen-agentTemplate.yaml").toString()
-        //result = sh(returnStdout: true, script: "set -x && yq gen-agentTemplate.yaml")
-       // return result
-        //#sed -i '1d' tmp-podagent.yaml #workartund
-        //result = readYaml file: 'gen-agentTemplate.yaml'
-
-        //println "GEN-AGENT"
-        //println result
-
-        /* sh "rm -v ${podTemplateFilePath}"
-         */
     }
-
 }
 
+def replaceTokens (string,replacements) {
+    replacements.each { token, value ->
+        string = string.replace(token, value)
+    }
+    return string
+}
 
