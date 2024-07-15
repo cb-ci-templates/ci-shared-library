c0dc69d399a11c101d6d60d807b72034c03c8eb5
cccaternberg <acaternberg@cloudbees.com>
2024-07-15 09:11:37 +0200

    sed

--- vars/init.groovy
+++ vars/init.groovy
@@ -7,8 +7,10 @@ def call(Map configDefaults) {
     //Optional:merge with global  defaults
     container("yq") {
         writeYaml file: 'ci-config-defaults.yaml', data: libraryResource("json/ci-config-defaults.yaml")
-        sh """
+        sh """            
+            sed -i '/|-/d' ci-config-defaults.yaml
             cat ci-config-defaults.yaml
+            sed -i '/|-/d' ${configDefaults.branchPropertiesFile} 
             cat ${configDefaults.branchPropertiesFile}                      
             yq eval-all 'select(fileIndex == 0) * select(fileIndex == 1)' ${configDefaults.branchPropertiesFile} ci-config-defaults.yaml > config-merged.yaml
             sed -i '/---/d' config-merged.yaml
