bc17735c18e4be9b7150dd79e3896c940eef9ec5
cccaternberg <acaternberg@cloudbees.com>
2024-07-15 13:57:15 +0200

    fix param

--- vars/pipeline_simple2.groovy
+++ vars/pipeline_simple2.groovy
@@ -67,7 +67,7 @@ def call(Map configDefaults) {
                 agent {
                     kubernetes {
                         //yaml "${agentYaml}"
-                        yamlFile config.dynamicPodTemplateFile
+                        yamlFile  libraryResource("podtemplates/podTemplate-envsubst-images.yaml")
                     }
                 }
                 stages {
