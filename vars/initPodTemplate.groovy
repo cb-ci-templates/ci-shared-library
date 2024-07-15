9a686efc7e532eeb1c76f7dcdc38627e1087703d
cccaternberg <acaternberg@cloudbees.com>
2024-07-15 16:20:36 +0200

    add custom yq image

--- vars/initPodTemplate.groovy
+++ vars/initPodTemplate.groovy
@@ -3,44 +3,11 @@ def call(Map config) {
     container("yq") {
         // Define the path to your template file
         def podTemplateFilePath = 'agentTemplate.yaml'
+        env.MAVEN_IMAGE = config.build.maven.image
         //env.MAVEN_IMAGE="maven:3-amazoncorretto-17"
         //agentRef=libraryResource("podtemplates/podTemplate-envsubst-images.yaml")
         writeYaml file: podTemplateFilePath, data: libraryResource("podtemplates/podTemplate-envsubst-images.yaml")
-        // Define the environment variables
-        def envVars = [
-                MAVEN_IMAGE = config.build.maven.image
-        ]
-     /* TOD=: itearet over configv images and add to array
-        def envVars = [
-                MAVEN_IMAGE = config.build.maven.image,
-                GRADLE_IMAGE = config.build.maven.image
-        ]
-
-      */
-
-
-
-
-
-
-// Replace variables in the file
-         agentYaml = replaceVariablesInFile(podTemplateFilePath, envVars)
-
-// Print the result or save it back to a file
-        println(agentYaml)
-        // Optionally, save the result to a new file
-// new File('output.yaml').text = result
-        writeYaml file: 'gen-agentTemplate.yaml', data: agentYaml
-
-
-
-
-
-
-
-
-
-       /* sh """
+        sh """
             #cat agentTemplate.yaml |envsubst |yq > gen-agentTemplate.yaml
             ls -la
             sed -i "s/^  //g" agentTemplate.yaml 
@@ -48,11 +15,10 @@ def call(Map config) {
             cat agentTemplate.yaml
             envsubst < agentTemplate.yaml > gen-agentTemplate.yaml
             ls -la            
-         """
-
-        */
+        """
         //#sed -i '1d' tmp-podagent.yaml #workartund
         agentYaml = readYaml file: 'gen-agentTemplate.yaml'
+        println "GEN-AGENT"
         println agentYaml
         archiveArtifacts artifacts: '*.yaml', followSymlinks: false
 
@@ -65,12 +31,3 @@ def call(Map config) {
 }
 
 
-def replaceVariablesInFile(filePath, envVars) {
-    // Read the content of the file
-    def fileContent = readYaml file: filePath
-    // Replace placeholders with environment variables
-    envVars.each { key, value ->
-        fileContent = fileContent.replace("\${${key}}", value)
-    }
-    return fileContent
-}
