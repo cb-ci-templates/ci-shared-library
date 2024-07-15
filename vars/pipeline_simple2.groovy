2ae0657976e479bb70016b032a51b9bc870f35d1
cccaternberg <acaternberg@cloudbees.com>
2024-07-15 09:45:50 +0200

    fix param

--- vars/pipeline_simple2.groovy
+++ vars/pipeline_simple2.groovy
@@ -13,8 +13,25 @@ def call(Map configDefaults) {
                 steps {
                     script {
                         config = init configDefaults
-                        agentYaml = initPodTemplate config
-                        println agentYaml
+                        //agentYaml = initPodTemplate config
+                        //println agentYaml
+                    }
+                    container("envsubst") {
+                        //TODO: Iterate over all config.build.X.images and expose them as ebv vars
+                        env.MAVEN_IMAGE = config.build.maven.image
+                        //env.MAVEN_IMAGE="maven:3-amazoncorretto-17"
+                        writeYaml file: 'agent.yaml', data: libraryResource("podtemplates/podTemplate-envsubst-images.yaml")
+                        sh '''
+                            ls -la 
+                            cat agent.yaml
+                            envsubst < agent.yaml > tmp-podagent.yaml
+                            sed -i '1d' tmp-podagent.yaml #workartund
+                            cat tmp-podagent.yaml
+                        '''
+                        script{
+                            agentYaml = readYaml file: "tmp-podagent.yaml"
+                        }
+                        sh "echo $agentYaml"
                     }
                 }
             }
