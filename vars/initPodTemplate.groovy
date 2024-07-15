c8abb1d9f6b858756da646a69d72d3cf9a47f486
cccaternberg <acaternberg@cloudbees.com>
2024-07-15 09:54:28 +0200

    fix param

--- vars/initPodTemplate.groovy
+++ vars/initPodTemplate.groovy
@@ -6,12 +6,12 @@ def call(Map config) {
         //env.MAVEN_IMAGE="maven:3-amazoncorretto-17"
         writeYaml file: 'agent.yaml', data: libraryResource("podtemplates/podTemplate-envsubst-images.yaml")
         sh '''
-                            ls -la 
-                            cat agent.yaml
-                            envsubst < agent.yaml > tmp-podagent.yaml
-                            sed -i '1d' tmp-podagent.yaml #workartund
-                            cat tmp-podagent.yaml
-                        '''
+            ls -la 
+            cat agent.yaml
+            envsubst < agent.yaml > tmp-podagent.yaml
+            sed -i '1d' tmp-podagent.yaml #workartund
+            cat tmp-podagent.yaml
+        '''
         agentYaml = readYaml file: "tmp-podagent.yaml"
         sh '''
            rm -v tmp-podagent.yaml
--- vars/pipeline_simple2.groovy
+++ vars/pipeline_simple2.groovy
@@ -13,26 +13,9 @@ def call(Map configDefaults) {
                 steps {
                     script {
                         config = init configDefaults
-                        //agentYaml = initPodTemplate config
-                        //println agentYaml
+                        agentYaml = initPodTemplate config
                     }
-                    container("envsubst") {
-                        //TODO: Iterate over all config.build.X.images and expose them as ebv vars
-                        //env.MAVEN_IMAGE="maven:3-amazoncorretto-17"
-                        writeYaml file: 'agent.yaml', data: libraryResource("podtemplates/podTemplate-envsubst-images.yaml")
-                        sh '''
-                            ls -la 
-                            cat agent.yaml
-                            envsubst < agent.yaml > tmp-podagent.yaml
-                            sed -i '1d' tmp-podagent.yaml #workartund
-                            cat tmp-podagent.yaml
-                        '''
-                        script{
-                            env.MAVEN_IMAGE = config.build.maven.image
-                            agentYaml = readYaml file: "tmp-podagent.yaml"
-                        }
-                        sh "echo $agentYaml"
-                    }
+                    sh "echo ${agentYaml}"
                 }
             }
             stage('CI') {
