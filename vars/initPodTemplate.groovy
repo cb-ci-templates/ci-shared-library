c74c1b7564e0ceb08873809ef4af64153eb15dd4
cccaternberg <acaternberg@cloudbees.com>
2024-07-15 09:36:01 +0200

    fix param

--- vars/initPodTemplate.groovy
+++ vars/initPodTemplate.groovy
@@ -1,22 +1,19 @@
 def call(Map config) {
-    def result=null
     container("envsubst") {
         //TODO: Iterate over all config.build.X.images and expose them as ebv vars
         env.MAVEN_IMAGE = config.build.maven.image
         //env.MAVEN_IMAGE="maven:3-amazoncorretto-17"
-        writeYaml file: "agent.yaml" , data: libraryResource("podtemplates/podTemplate-envsubst-images.yaml")
+        writeYaml file: 'agent.yaml', data: libraryResource("podtemplates/podTemplate-envsubst-images.yaml")
         sh '''
-            ls -la 
-            cat agent.yaml
-            envsubst < agent.yaml > tmp-agent.yaml
-            sed -i -e '1d'  tmp-agent.yaml 
-            sed -i '/|/d' tmp-agent.yaml #workartund to remove "|"
-            cat tmp-agent.yaml
-        '''
-        //writeYaml file: config.dynamicPodTemplateFile , data: libraryResource("podtemplates/podTemplate-envsubst-images.yaml")
-        result = readYaml file: "tmp-agent.yaml"
-        sh '''echo $result
-           rm tmp-agent.yaml
+                            ls -la 
+                            cat agent.yaml
+                            envsubst < agent.yaml > tmp-podagent.yaml
+                            sed -i '1d' tmp-podagent.yaml #workartund
+                            cat tmp-podagent.yaml
+                        '''
+        agentYaml = readYaml file: "tmp-podagent.yaml"
+        sh '''
+           rm -v tmp-agent.yaml
         '''
 
     }
