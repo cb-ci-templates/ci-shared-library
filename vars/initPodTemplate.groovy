de0afcdb42c98cfd8a3799da5a3cc12ab784299e
cccaternberg <acaternberg@cloudbees.com>
2024-07-15 15:08:52 +0200

    fix param

--- vars/initPodTemplate.groovy
+++ vars/initPodTemplate.groovy
@@ -1,13 +1,13 @@
 def call(Map config) {
     def agentYaml=null
-    container("envsubst") {
+    container("yq") {
         //TODO: Iterate over all config.build.X.images and expose them as ebv vars
         env.MAVEN_IMAGE = config.build.maven.image
         //env.MAVEN_IMAGE="maven:3-amazoncorretto-17"
-        agentRef=libraryResource("podtemplates/podTemplate-envsubst-images.yaml")
-        //writeYaml file: 'agentTemplate.yaml', data: libraryResource("podtemplates/podTemplate-envsubst-images.yaml")
+        //agentRef=libraryResource("podtemplates/podTemplate-envsubst-images.yaml")
+        writeYaml file: 'agentTemplate.yaml', data: libraryResource("podtemplates/podTemplate-envsubst-images.yaml")
         sh """
-            echo ${agentRef} |envsubst > gen-agentTemplate.yaml
+            cat agentTemplate.yaml |envsubst |yq > gen-agentTemplate.yaml
             ls -la
             sed -i "s/^  //g" agentTemplate.yaml 
             sed -i '1d' agentTemplate.yaml 
@@ -16,17 +16,16 @@ def call(Map config) {
             ls -la            
          """
         //#sed -i '1d' tmp-podagent.yaml #workartund
-        archiveArtifacts artifacts: '*.yaml', followSymlinks: false
        // agentYaml = readYaml file: 'gen-agentTemplate.yaml'
-    }
-    container ("yq"){
         sh "yq gen-agentTemplate.yaml"
         agentYaml = readYaml file: 'gen-agentTemplate.yaml'
         println agentYaml
-       /* sh '''
-           rm -v agent.yaml
-        '''
-        */
+        archiveArtifacts artifacts: '*.yaml', followSymlinks: false
+
+        /* sh '''
+            rm -v agent.yaml
+         '''
+         */
     }
     return agentYaml
 }
