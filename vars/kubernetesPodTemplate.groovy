0fad3f87037b1e3978c52d31c72a286b7a87bb0c
cccaternberg <acaternberg@cloudbees.com>
2024-07-15 17:30:40 +0200

    add custom yq image

--- vars/kubernetesPodTemplate.groovy
+++ vars/kubernetesPodTemplate.groovy
@@ -1,9 +1,11 @@
 // vars/kubernetesPodTemplate.groovy
 def call(Map config=[:]) {
     def template = libraryResource("podtemplates/${config.file}")
+    /*TODO: iterate over images from ci-config
     if (config.binding) {
         def engine = new groovy.text.GStringTemplateEngine()
         template = engine.createTemplate(template).make(config.binding).toString()
     }
+     */
     return template
 }
