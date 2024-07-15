97ada215c58901d788a23ff944527be64c3f6599
cccaternberg <acaternberg@cloudbees.com>
2024-07-15 22:32:38 +0200

    add custom yq image

--- vars/initPodTemplate.groovy
+++ vars/initPodTemplate.groovy
@@ -19,8 +19,8 @@ def call(Map config) {
         // Function to check if a YAML path exists
         def images=sh(script: '''
                 set -x
-                yq -o=json '.' agentTemplate.yaml |jq -r 'paths |join(".")'
-                for img in \$(yq -o=json '.' agentTemplate.yaml |jq -r 'paths |join(".")' |grep -E "build.*.image")
+                yq eval -o=json '.' agentTemplate.yaml |jq -r 'paths |join(".")'
+                for img in \$(yq eval -o=json '.' agentTemplate.yaml |jq -r 'paths |join(".")' |grep -E "build.*.image")
                 do
                     echo \$img
                 done
