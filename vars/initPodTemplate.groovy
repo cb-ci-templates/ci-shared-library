8495336961f851072b8a43ee6dd060abf0e30111
cccaternberg <acaternberg@cloudbees.com>
2024-07-15 13:07:34 +0200

    fix param

--- vars/initPodTemplate.groovy
+++ vars/initPodTemplate.groovy
@@ -4,18 +4,18 @@ def call(Map config) {
         //TODO: Iterate over all config.build.X.images and expose them as ebv vars
         env.MAVEN_IMAGE = config.build.maven.image
         //env.MAVEN_IMAGE="maven:3-amazoncorretto-17"
-        writeYaml file: 'agent.yaml', data: libraryResource("podtemplates/podTemplate-envsubst-images.yaml")
+        writeYaml file: 'agentTemplate.yaml', data: libraryResource("podtemplates/podTemplate-envsubst-images.yaml")
         sh """
             ls -la 
             cat agent.yaml
-            envsubst <agent.yaml> tmp-podagent.yaml
-            sed -i '1d' tmp-podagent.yaml
-            sed -i "s/^  //g" tmp-podagent.yaml
+            envsubst <agentTemplate.yaml |yq eval > agentTemplate.yaml
+            sed -i '1d' agentTemplate.yaml
+            sed -i "s/^  //g" agentTemplate.yaml
             ls -la            
          """
         //#sed -i '1d' tmp-podagent.yaml #workartund
         archiveArtifacts artifacts: '*.yaml', followSymlinks: false
-        agentYaml = readYaml file: "tmp-podagent.yaml"
+        agentYaml = readYaml file: 'agentTemplate.yaml'
         println agentYaml
        /* sh '''
            rm -v agent.yaml
