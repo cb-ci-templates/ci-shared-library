42a71ee98dc4db5e6ea0bc18a75a2f58af079299
cccaternberg <acaternberg@cloudbees.com>
2024-10-10 12:56:00 +0200

    v1

--- vars/pipelineReadTrusted.groovy
+++ vars/pipelineReadTrusted.groovy
@@ -1,44 +1,45 @@
-library '_github.com_pipeline-demo-caternberg_pipeline_examples' _
-podTemplate(containers: [
-        containerTemplate(name: 'shell', image: 'busybox', command: 'sleep', args: '99d')
-]) {
-    node(POD_LABEL) {
+def call(Map config) {
+    podTemplate(containers: [
+            containerTemplate(name: 'shell', image: 'busybox', command: 'sleep', args: '99d')
+    ]) {
+        node(POD_LABEL) {
 
-        /**
-         Pre exec stage with branch decision
-         */
-        stage('Injected-pre-stage') {
-            container('shell') {
-                echo 'Implement your pre-execution steps below......'
-                /*
+            /**
+             Pre exec stage with branch decision
+             */
+            stage('Injected-pre-stage') {
+                container('shell') {
+                    echo 'Implement your pre-execution steps below......'
+                    /*
                 Implement your pre-execution steps here......
                 */
 
 
-                if (env.BRANCH_NAME == 'master') {
-                    echo 'I only execute on the master branch'
-                } else  if (env.BRANCH_NAME.startsWith('feature')) {
-                    echo "I execute on $BRANCH_NAME branch"
-                } else {
-                    echo "I execute elsewhere"
+                    if (env.BRANCH_NAME == 'master') {
+                        echo 'I only execute on the master branch'
+                    } else if (env.BRANCH_NAME.startsWith('feature')) {
+                        echo "I execute on $BRANCH_NAME branch"
+                    } else {
+                        echo "I execute elsewhere"
+                    }
                 }
             }
-        }
-        /**
-         Here we inject the Jenkinsfile from the developer repo branch
-         */
-        evaluate (readTrusted ("Jenkinsfile"))
+            /**
+             Here we inject the Jenkinsfile from the developer repo branch
+             */
+            evaluate(readTrusted("Jenkinsfile"))
 
-        /**
-         Post exec stage
-         */
-        stage('Injected-post-stage') {
-            container('shell') {
-                echo 'Implement your post-execution steps below......'
-                /*
+            /**
+             Post exec stage
+             */
+            stage('Injected-post-stage') {
+                container('shell') {
+                    echo 'Implement your post-execution steps below......'
+                    /*
                     Implement your post-execution steps here......
                     E.g QA scans that should be executed in each Pipeline run
                 */
+                }
             }
         }
     }
