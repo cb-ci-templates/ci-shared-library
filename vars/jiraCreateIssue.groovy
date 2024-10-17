6a1e8af8a9bf423ff0844a4fbf9bea69a043a0a9
cccaternberg <acaternberg@cloudbees.com>
2024-10-17 20:03:19 +0200

    add

--- vars/jiraCreateIssue.groovy
+++ vars/jiraCreateIssue.groovy
@@ -1,7 +1,6 @@
 def call(String jiraToken, Map config=[:]) {
     config["JIRA_TOKEN"]=jiraToken
     sh """
-    set -x
     cat << EOF > createIssue.json
     {
        "fields": {
@@ -21,7 +20,9 @@ def call(String jiraToken, Map config=[:]) {
     EOF
     ls -la
     cat createIssue.json
-    curl -v  -D- -u ${config.JIRA_TOKEN} -X POST --data @createIssue.json -H "Content-Type: application/json" ${config.JIRA_URL}/rest/api/2/issue
+    """
+    sh """
+    curl -D- -u ${config.JIRA_TOKEN} -X POST --data @createIssue.json -H "Content-Type: application/json" ${config.JIRA_URL}/rest/api/2/issue
     archiveArtifacts artifacts: '*.*', followSymlinks: false
     """
 
