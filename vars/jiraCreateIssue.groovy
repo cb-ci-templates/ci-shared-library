da4b6a03b40e9c122db18677f0e5ade24727f9ed
cccaternberg <acaternberg@cloudbees.com>
2024-10-17 18:27:20 +0200

    add

--- vars/jiraCreateIssue.groovy
+++ vars/jiraCreateIssue.groovy
@@ -1,12 +1,13 @@
 def call(Map config=[:]) {
-    writeYaml file: 'createIssueTemplate.json', data: libraryResource("jira/createIssue.json")
-    env.JIRA_KEY=config.JIRA_KEY
-    env.JIRA_ISSUE_TYPE_NAME=config.JIRA_ISSUE_TYPE_NAME
-    env.JIRA_DESCRIPTION=config.JIRA_DESCRIPTION
-    env.JIRA_SUMMARY=config.JIRA_SUMMARY
-    env.JIRA_URL=config.JIRA_URL
-    env.JIRA_TOKEN==config.JIRA_TOKEN
-    sh "envsubst < createIssueTemplate.json  > createIssue.json"
-    def render = renderTemplate(rawBody,binding)
-    sh('curl -D- -u $JIRA_CREDENTIALS -X POST --data createIssue.json -H "Content-Type: application/json" $JIRA_URL/rest/api/2/issue')
+    writeYaml file: 'jiraCreateIssue.sh', data: libraryResource("jira/createIssue.sh")
+    sh """
+     chmod 755 jiraCreateIssue.sh
+     jiraCreateIssue.sh \
+        ${config.JIRA_KEY} \
+        ${config.JIRA_ISSUE_TYPE} \
+        ${config.JIRA_DESCRIPTION} \
+        ${config.JIRA_SUMMARY} \
+        ${config.JIRA_URL} \
+        ${config.JIRA_TOKEN} \
+    """
 }
