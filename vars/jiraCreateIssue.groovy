b1a96ad5b582982f2ef796648d1f930d015bb364
cccaternberg <acaternberg@cloudbees.com>
2024-10-17 19:04:11 +0200

    add

--- vars/jiraCreateIssue.groovy
+++ vars/jiraCreateIssue.groovy
@@ -4,11 +4,11 @@ def call(String jiraToken, Map config=[:]) {
      chmod 755 ./jiraCreateIssue.sh
      ls -la
      ./jiraCreateIssue.sh \
-        ${config.JIRA_KEY} \
-        ${config.JIRA_ISSUE_TYPE} \
-        ${config.JIRA_DESCRIPTION} \
-        ${config.JIRA_SUMMARY} \
-        ${config.JIRA_URL} \
-        ${jiraToken} \
+        "${config.JIRA_KEY}" \
+        "${config.JIRA_ISSUE_TYPE}" \
+        "${config.JIRA_DESCRIPTION}"\
+        "${config.JIRA_SUMMARY}" \
+        "${config.JIRA_URL}" \
+        "${jiraToken}" \
     """
 }
