77a0ea217aeae8673ac44d0baa39dc2fc91cfa70
cccaternberg <acaternberg@cloudbees.com>
2024-10-17 20:08:49 +0200

    add

---
+++ testPipelines/ci-jira-integration/Jenkinsfile.groovy
@@ -0,0 +1,51 @@
+library identifier: 'ci-shared-library@main', retriever: modernSCM(
+        [$class: 'GitSCMSource',
+         remote: 'https://github.com/cb-ci-templates/ci-shared-library.git'])
+
+// Building the data object
+def configYaml = """---
+JIRA_KEY : 'SCRUM'
+JIRA_ISSUE_TYPE : 'Task'
+JIRA_DESCRIPTION: 'TEST'
+JIRA_SUMMARY: 'TEST'
+JIRA_URL: 'https://cloudbees-acaternberg-test.atlassian.net/'
+"""
+Map configMap = readYaml text: "${configYaml}"
+println configMap
+pipeline {
+    agent {
+        kubernetes {
+            yaml '''
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+    - name: shell
+      image: curlimages/curl:latest
+      runAsUser: 1000
+      command:
+        - cat
+      tty: true
+      workingDir: "/home/jenkins/agent"
+      securityContext:
+        runAsUser: 1000
+'''
+            defaultContainer 'shell'
+            retries 2
+        }
+    }
+    stages {
+        stage('Main') {
+            steps {
+                container ("shell"){
+                    jiraComment body: 'This is my test comment', issueKey: configMap.JIRA_KEY
+                    withCredentials([usernamePassword(credentialsId: 'jira-user-token', passwordVariable: 'JIRA_PW', usernameVariable: 'JIRA_USER')]) {
+                        jiraCreateIssue ("${JIRA_USER}:${JIRA_PW}",configMap)
+                    }
+
+                    //step([$class: 'JiraVersionCreatorBuilder', jiraProjectKey: "${configMap.JIRA_KEY}", jiraVersion: 'newversion'])
+                }
+            }
+        }
+    }
+}
