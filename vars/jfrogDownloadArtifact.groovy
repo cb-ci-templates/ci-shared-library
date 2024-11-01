995e414a67791234633b297b2633c55ed0930b10
Shaohua Wen <shaohua.wen@gmail.com>
2024-11-01 16:28:42 +0700

    jfrog library

--- vars/jfrogDownloadArtifact.groovy
+++ vars/jfrogDownloadArtifact.groovy
@@ -4,11 +4,7 @@ def call(Map config=[:]) {
     withCredentials([string(credentialsId: 'jfrog-user-token', variable: 'API_TOKEN')]) {
 
         sh """
-            export ARTIFACTORY_URL=${config.ARTIFACTORY_URL}
-            export REPO_NAME=${config.REPO_NAME}
-            export ARTIFACT_PATH=${config.ARTIFACT_PATH}
-            
-            curl -H "Authorization: Bearer ${API_TOKEN}" -O "${ARTIFACTORY_URL}/artifactory/${REPO_NAME}/${ARTIFACT_PATH}"
+            curl -H "Authorization: Bearer ${API_TOKEN}" -O "${config.ARTIFACTORY_URL}/artifactory/${config.REPO_NAME}/${config.ARTIFACT_PATH}"
         """
         archiveArtifacts artifacts: '*.*', followSymlinks: false
     }
