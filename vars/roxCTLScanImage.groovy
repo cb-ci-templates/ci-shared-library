73039bf80898c4389b53a9ad50c5d26e52592941
Shaohua Wen <shaohua.wen@gmail.com>
2024-11-04 11:41:06 +0700

    use env. for env variables

--- vars/roxCTLScanImage.groovy
+++ vars/roxCTLScanImage.groovy
@@ -5,23 +5,19 @@ def call(Map config=[:]) {
 
         sh """
 export ROX_API_TOKEN="${API_TOKEN}"
-ROX_CENTRAL_ENDPOINT="${config.ENDPOINT}"
-INSECURE_SKIP_TLS_VERIFY="${config.SKIP_TLS_VERIFY}"
-IMAGE_REGISTRY="${config.IMAGE_REGISTRY}"
-IMAGE_NAME="${config.IMAGE_NAME}"
 
-curl -s -k -L -H "Authorization: Bearer ${env.ROX_API_TOKEN}" \\
-  "https://${env.ROX_CENTRAL_ENDPOINT}/api/cli/download/roxctl-linux" \\
+curl -s -k -L -H "Authorization: Bearer ${API_TOKEN}" \\
+  "https://${config.ENDPOINT}/api/cli/download/roxctl-linux" \\
   --output ./roxctl  \\
   > /dev/null
 
 chmod +x ./roxctl  > /dev/null
 
-if [ "${env.INSECURE_SKIP_TLS_VERIFY}" = "true" ]; then
+if [ "${config.SKIP_TLS_VERIFY}" = "true" ]; then
   export ROX_INSECURE_CLIENT_SKIP_TLS_VERIFY=true
 fi
 
-./roxctl image scan -e "${env.ROX_CENTRAL_ENDPOINT}" --image "${env.IMAGE_REGISTRY}/${env.IMAGE_NAME}"
+./roxctl image scan -e "${config.ENDPOINT}" --image "${config.IMAGE_REGISTRY}/${config.IMAGE_NAME}"
         """
     }
 
