592d05e0c630deb890b2b89678c2302a52ef9419
Shaohua Wen <shaohua.wen@gmail.com>
2024-11-04 12:39:07 +0700

    add debug

--- vars/roxCTLScanImage.groovy
+++ vars/roxCTLScanImage.groovy
@@ -2,9 +2,13 @@ package vars
 
 def call(Map config=[:]) {
     withCredentials([string(credentialsId: 'roxctl-user-token', variable: 'ROX_API_TOKEN')]) {
-        sh """
+        def arch = sh(script: '''
+arch="$(uname -m | sed "s/x86_64//" | sed "s/aarch/arm/")"
+arch="${arch:+-$arch}"
+echo "$arch"
+    ''', returnStdout: true).trim()
 
-arch="\$(uname -m | sed "s/x86_64//" | sed "s/aarch/arm/")"; arch="${arch:+-$arch}"
+        sh """
 #curl -L -f -o roxctl "https://mirror.openshift.com/pub/rhacs/assets/4.5.4/bin/Linux/roxctl${arch}"
 
 curl -s -k -L -H "Authorization: Bearer ${ROX_API_TOKEN}" \\
