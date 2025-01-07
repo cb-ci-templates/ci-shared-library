efcaaf329ed29525d35270c2e6b724572b4b5631
cccaternberg <acaternberg@cloudbees.com>
2025-01-07 15:38:23 +0100

    update maven version

--- vars/kaniko.groovy
+++ vars/kaniko.groovy
@@ -1,4 +1,4 @@
-def call(config, image_tag) {
+def call(config) {
     env.DOCKER_CONFIG = "/kaniko/.docker"
     container(name: 'kaniko', shell: '/busybox/sh') {
         withCredentials([file(credentialsId: "${CRED_DOCKER_CRED_ID}", variable: 'DOCKER_CONFIG_JSON_FILE')]) {
@@ -6,11 +6,13 @@ def call(config, image_tag) {
                 cp ${DOCKER_CONFIG_JSON_FILE} ${DOCKER_CONFIG}/config.json
             '''
             echo "Deploy to  : ${config.ci.kaniko.registry}/${config.ci.kaniko.application_image}:${image_tag}"
+            //expose SHORT_COMMIT
+            gitShortCommit 7
             sh label: 'kanikoBuildAndPush', script: """              
                 export https_proxy="${config.ci.https_proxy}"
                 export no_proxy="${config.ci.no_proxy}"
                 /kaniko/executor  --dockerfile Dockerfile --insecure --skip-tls-verify --cache=false  --context . \
-                --destination ${config.ci.kaniko.registry}/${config.ci.kaniko.application_image}:${image_tag} \
+                --destination ${config.ci.kaniko.registry}/${config.ci.kaniko.application_image}:${SHORT_COMMIT} \
                 --destination ${config.ci.kaniko.registry}/${config.ci.kaniko.application_image}:latest #\
 
                 #--destination ${config.ci.kaniko.registry_deploy}/${config.ci.kaniko.application_image}:${image_tag} #\
