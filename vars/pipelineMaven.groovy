86f1adac76592f37b999e934cafa89ec5cee0ff1
cccaternberg <acaternberg@cloudbees.com>
2025-01-07 13:32:06 +0100

    add

--- vars/pipelineMaven.groovy
+++ vars/pipelineMaven.groovy
@@ -36,35 +36,10 @@ def call(Map configDefaults) {
                 stages {
                     stage("build") {
                         steps {
-                            container("maven") {
-                                //https://plugins.jenkins.io/pipeline-maven/
-                                withMaven(
-                                        //Use `$WORKSPACE/.repository` for local repository folder to avoid shared repositories
-                                        //consider to use CloudBees Workspace caching https://docs.cloudbees.com/docs/cloudbees-ci/latest/pipelines/cloudbees-cache-step
-                                        mavenLocalRepo: '/tmp/.m2',
-                                        // Maven settings.xml file defined with the Jenkins Config File Provider Plugin
-                                        // We recommend to define Maven settings.xml globally at the folder level using
-                                        // navigating to the folder configuration in the section "Pipeline Maven Configuration / Override global Maven configuration"
-                                        // or globally to the entire master navigating to  "Manage Jenkins / Global Tools Configuration"
-                                        //mavenSettingsConfig: 'global-maven-settings'
-                                ) {
-                                    //sh config.build.maven.steps[1]
-                                    script {
-                                        config.build.maven.steps.collect { step ->
-                                            println step
-                                            sh step
-                                        }
-                                    }
-
-                                    //https://docs.cloudbees.com/docs/cloudbees-ci/latest/pipelines/cloudbees-cache-step
-                                    //writeCache name: 'maven-repo', includes: '.m2/repository/**', excludes: '**SNAPSHOT**'
-                                }
-                                junit '**/target/surefire-reports/TEST-*.xml'
-                                archive '**/target/*.jar'
-                            }
+                            build comfig
                         }
                     }
-                    stage("Docker") {
+                    stage("Image") {
                         when {
                             branch 'main'
                         }
