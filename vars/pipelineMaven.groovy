c568b7b83dc5ead930716e96ee95ee8a85c56ea9
cccaternberg <acaternberg@cloudbees.com>
2025-01-23 15:56:35 +0100

    coment

--- vars/pipelineMaven.groovy
+++ vars/pipelineMaven.groovy
@@ -19,7 +19,7 @@ def call(Map configDefaults) {
                 steps {
                     script {
                         config = init configDefaults
-                        agentYaml = initPodTemplate config
+                        //agentYaml = initPodTemplate config
                     }
                 }
             }
