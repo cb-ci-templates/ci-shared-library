00da9a9ce29a34624a95820422cf263e17f96e72
cccaternberg <acaternberg@cloudbees.com>
2025-04-17 10:50:15 +0200

    add proxy

--- vars/buildKaniko.groovy
+++ vars/buildKaniko.groovy
@@ -18,13 +18,15 @@ def call (Map config) {
                 kanikoProxy=" --build-arg HTTP_PROXY=${env.HTTPS_PROXY} --build-arg HTTPS_PROXY=${env.HTTPS_PROXY}  --build-arg http_proxy=${env.HTTPS_PROXY}  --build-arg https_proxy=${env.HTTPS_PROXY} "
                 println kanikoProxy
             }
-            sh label: 'kanikoBuildAndPush', script: """              
-                #export https_proxy="${config.ci.https_proxy}"
-                #export no_proxy="${config.ci.no_proxy}"
+            sh label: 'kanikoBuildAndPush', script: """   
+                env  |grep -i proxy |sprt           
+                export https_proxy="${env.https_proxy}"
+                export no_proxy="${env.no_proxy}"
+                env  |grep -i proxy |sprt 
                 /kaniko/executor  --dockerfile Dockerfile --insecure --skip-tls-verify --cache=false  --context . \
                 --destination ${config.ci.kaniko.registry}/${config.ci.kaniko.application_image}:${SHORT_COMMIT} \
                 --destination ${config.ci.kaniko.registry}/${config.ci.kaniko.application_image}:latest #\
-                ${kanikoProxy} 
+                # ${kanikoProxy} 
                 #--destination ${config.ci.kaniko.registry_deploy}/${config.ci.kaniko.application_image}:${SHORT_COMMIT} #\
                 #--build-arg HTTP_PROXY=${config.ci.https_proxy} \
                 #--build-arg HTTPS_PROXY=${config.ci.https_proxy} \
