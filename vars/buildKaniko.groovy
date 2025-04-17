75a132478697490b03fc3e202ff944e08dfa0aae
cccaternberg <acaternberg@cloudbees.com>
2025-04-17 11:09:33 +0200

    add proxy

--- vars/buildKaniko.groovy
+++ vars/buildKaniko.groovy
@@ -6,7 +6,7 @@ def call (Map config) {
     }
     env.DOCKER_CONFIG = "/kaniko/.docker"
     container(name: 'kaniko', shell: '/busybox/sh') {
-        //withCredentials([file(credentialsId: 'dockerconfig', variable: 'DOCKER_CONFIG_JSON_FILE')]) {
+        // credentialsId: dockerconfig -  dockerconfig.json as "secret text" credentials
         withCredentials([string(credentialsId: 'dockerconfig', variable: 'DOCKER_CONFIG_JSON')]) {
             sh label: 'kanikoPrepareConfig', script: '''
                 echo ${DOCKER_CONFIG_JSON} > ${DOCKER_CONFIG}/config.json
