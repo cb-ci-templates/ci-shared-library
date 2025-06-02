60693255ff72eff320a0586f81dab3d640a67862
cccaternberg <acaternberg@cloudbees.com>
2025-06-02 15:53:38 +0200

    add

--- vars/routerBuildImage.groovy
+++ vars/routerBuildImage.groovy
@@ -1,5 +1,6 @@
 def call(config) {
     //TODO: Check the path in ci-config if kaniko is enabled/defined
-    //MAYBE some if then else
-    buildKaniko config
+    if (config.ci.kaniko){
+        buildKaniko config
+    }
 }
